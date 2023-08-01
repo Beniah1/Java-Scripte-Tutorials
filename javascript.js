bd1b706c887720847347e81c332ee9add531fe3d
Beniah1 <diallobeniah@gmail.com>
2023-08-01 13:35:14 +0000

    Version 6

--- javascript.js
+++ javascript.js
@@ -1,3 +1,52 @@
-document.write('Yoko')
-document.write('How you doing!')
-console.log("This is a string")
+// String variables
+let name = "John";
+let city = "New York";
+let country = "United States";
+let hobby = "Reading";
+let jobTitle = "Software Engineer";
+
+// Integer variables
+let age = 30;
+let score = 85;
+let numberOfFriends = 500;
+let numberOfApples = 10;
+let numberOfCars = 3;
+
+// Float variables
+let pi = 3.14159;
+let temperature = 25.5;
+let weight = 68.75;
+let height = 1.75;
+let averageScore = 92.5;
+
+// Boolean variables
+let isStudent = true;
+let hasPet = false;
+let isSunny = true;
+let hasCar = true;
+let isMarried = false;
+
+// Printing out the variables
+document.write("Name: " + name + "<br>");
+document.write("City: " + city + "<br>");
+document.write("Country: " + country + "<br>");
+document.write("Hobby: " + hobby + "<br>");
+document.write("Job Title: " + jobTitle + "<br>");
+
+document.write("Age: " + age + "<br>");
+document.write("Score: " + score + "<br>");
+document.write("Number of Friends: " + numberOfFriends + "<br>");
+document.write("Number of Apples: " + numberOfApples + "<br>");
+document.write("Number of Cars: " + numberOfCars + "<br>");
+
+document.write("Pi: " + pi + "<br>");
+document.write("Temperature: " + temperature + "<br>");
+document.write("Weight: " + weight + "<br>");
+document.write("Height: " + height + "<br>");
+document.write("Average Score: " + averageScore + "<br>");
+
+document.write("Is Student: " + isStudent + "<br>");
+document.write("Has Pet: " + hasPet + "<br>");
+document.write("Is Sunny: " + isSunny + "<br>");
+document.write("Has Car: " + hasCar + "<br>");
+document.write("Is Married: " + isMarried + "<br>");
