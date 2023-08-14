15186f691131c915ae50f8f8defc634b976b3717
Beniah1 <diallobeniah@gmail.com>
2023-08-14 23:51:24 +0000

    Practice

--- conditionals.js
+++ conditionals.js
@@ -6,3 +6,9 @@ function addNumbers(num1, num2, num3) {
 // Call the function and log the answer
 let sum = addNumbers(2, 4, 6);
 console.log(sum);
+
+// Path: conditionals.js
+function home(){
+  console.log("I am at home");
+  document.innerHTML = "I am at home";
+}
